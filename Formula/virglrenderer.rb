4c0a968c6ab2b382d941b64f08ba355f42764f9c
startergo <7897244+startergo@users.noreply.github.com>
2026-01-14 05:21:44 +0000

    Update 1.0.33 source tarball sha256 to 49c02217d3e940695c78b0c68452660a13150ef7921890775552783a545b66b2

--- Formula/virglrenderer.rb
+++ Formula/virglrenderer.rb
@@ -5,7 +5,7 @@ class Virglrenderer < Formula
 
   version "1.0.33"
   url "https://github.com/startergo/homebrew-virglrenderer/archive/refs/tags/v1.0.33.tar.gz"
-  sha256 "aa03f2aa8018cfcf94875c63935816c4d11d23b610dad5777e5778320a9b2c4c"
+  sha256 "49c02217d3e940695c78b0c68452660a13150ef7921890775552783a545b66b2"
   head "https://gitlab.freedesktop.org/virgl/virglrenderer.git", branch: "main"
 
   bottle do
