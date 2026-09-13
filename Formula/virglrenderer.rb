e7daef144d249a38d6206153e6146539f893eae2
startergo <7897244+startergo@users.noreply.github.com>
2026-09-13 00:23:26 +0000

    Update to v1.0.42 with bottles

--- Formula/virglrenderer.rb
+++ Formula/virglrenderer.rb
@@ -4,9 +4,8 @@ class Virglrenderer < Formula
   license "MIT"
 
   version "1.0.42"
-  version "1.0.42"
-  url "https://github.com/startergo/homebrew-virglrenderer/archive/a19036bcc94aaff85f1ec70c25ee82c34e89898c.tar.gz"
-  sha256 "78203db6e96146f6d672efa01434441e53dc6c4506f9cfe5ff65e91986783060"
+  url "https://github.com/startergo/homebrew-virglrenderer/archive/refs/tags/v1.0.42.tar.gz"
+  sha256 "950273fbba46905b6112ee2bd0598c1da706c25319a7347058cbc52f04ba96dd"
   head "https://gitlab.freedesktop.org/virgl/virglrenderer.git", branch: "main"
 
   bottle do
